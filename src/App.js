fa1017e17458e6a7e6359d6b1baffac6c9e7429d
Nickolay Lyman <kolyalyman@gmail.com>
2021-03-20 12:00:01 +0200

    added rendering on condition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import ContactsList from './components/ContactsList';
 import ContactsFilter from './components/ContactsFilter';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getVisibleContacts } from './redux/selectors';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Button from './components/Button';
@@ -9,6 +11,7 @@ import st from './App.module.css';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const contacts = useSelector(getVisibleContacts);
 
   const togleModal = () => setShowModal(!showModal);
   return (
@@ -17,8 +20,8 @@ function App() {
       <Button onClick={togleModal} className={st.btn} value="Add  contact" />
       {showModal && <Modal onClose={togleModal} />}
       <h2 className={st.subtitle}>Contacts</h2>
-      <ContactsFilter />
-      <ContactsList />
+      {contacts.length > 1 && <ContactsFilter />}
+      {contacts.length > 0 && <ContactsList />}
       <ToastContainer
         position="top-center"
         autoClose={3000}
